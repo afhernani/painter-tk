00b011b5d8d3712caf2d600288723d9b056f0e97
afhernani <afernani@gmail.com>
2021-11-26 22:32:13 +0000

    svgcanvas objects paths

--- svgcanvas.py
+++ svgcanvas.py
@@ -52,7 +52,7 @@ def emit_warning(msg):
 	elif warnings_mode == MODULE:
 		from sys import stderr
 
-		stderr.write('canvas2svg warning: ')
+		stderr.write('sgvcanvas warning: ')
 		stderr.write(msg)
 		stderr.write('\n')
 
@@ -72,13 +72,19 @@ def configure(*flags):
 				"Please use one of constants: SEGMENT_TO_LINE, SEGMENT_TO_PATH"
 			)
 
+import logging
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger('svgcanvas')
+
+
 def calculateMethodName(attr):
     name=attr
     name=name.replace(':','_')
     name=name.replace('-','_')
     name='set_'+name
     return name
-    
+
+
 def setAttributes(attrs, obj):
     for attr in list(attrs.keys()):
         try:
@@ -89,7 +95,8 @@ def setAttributes(attrs, obj):
         except Exception:
             emit_warning(f"not found in")
 
-def addPolylineToCanvas(child_, object):
+
+def addPolylineToCanvas(child_, objects):
     nodeName_ = child_.nodeName
     if child_.hasAttributes():
         attrs = child_.attributes
@@ -102,7 +109,7 @@ def addPolylineToCanvas(child_, object):
                 # print(attr)
                options[attr]=attrs[attr].value
             
-            #print('options =', options)
+            log.info(f"options =, {options}")
             options_line = {}
             # options_line['points'] = [options['points']]
             options_line['fill'] = options['stroke']
@@ -118,11 +125,12 @@ def addPolylineToCanvas(child_, object):
             for coordenada in coordenadas:
                 cx, cy = coordenada.split(',')
                 datos.extend([float(cx), float(cy)])
-            print(datos)
-            object.create_line(datos, options_line)
-            print('create polyline')
+            log.info(f"datos = {datos}")
+            objects.create_line(datos, options_line)
+            log.info('create polyline')
 
-def addLineToCanvas(child_, object):
+
+def addLineToCanvas(child_, objects):
     nodeName_ = child_.nodeName
     if child_.hasAttributes():
         attrs = child_.attributes
@@ -142,24 +150,25 @@ def addLineToCanvas(child_, object):
                 else:
                     options[attr]=attrs[attr].value
             
-            print('options =', options)
+            # print('options =', options)
             options_line = {}
             options_line['fill'] = options['stroke']
             options_line['width'] = options['stroke-width']
             options_line['capstyle'] = ROUND # BUTT # options['stroke-linecap']
             options_line['smooth'] = True
             options_line['tags'] = 'line'
-            print('options_line =' , options_line)
+            # print('options_line =' , options_line)
             
             lista_puntos = [puntos_x[0], puntos_y[0], puntos_x[1], puntos_y[1]]
             '''for itemx in puntos_x:
                 for itemy in puntos_y:
                     lista_puntos.extend([itemx, itemy])'''
             
-            print(lista_puntos)
-            object.create_line(lista_puntos, options_line)
+            log.info(f"lista_puntos = {lista_puntos}")
+            objects.create_line(lista_puntos, options_line)
             # object.create_line(attrs['x1'].value, attrs['y1'].value, attrs['x2'].value, attrs['y2'].value)
-            print('create line')
+            log.info('create line')
+
 
 def addPathToCanvas(child_, objects):
     nodeName_ = child_.nodeName
@@ -169,7 +178,51 @@ def addPathToCanvas(child_, objects):
             options = {}
             for attr in list(attrs.keys()):
                 options[attr]=attrs[attr].value
-            print(options)
+            log.info(f"options = {options}")
+            # Nota:create interprete lexico sintactico.
+            log.info("Nota: pendiente de proceso")
+            graphs = addPathConstructor(options, objects)
+            log.info(graphs)
+            options['d']=graphs
+            log.info(options)
+            drawPaths(options, objects)
+
+
+def drawPaths(options, objects):
+    try:
+        log.info('drawPaths')
+    except Exception as ex:
+        log.info(ex.args)
+
+
+def addPathConstructor(options, objects):
+    """Analizar los objetos 'd' """
+    from iterationlexica import lex
+    obj = []
+    conjunto_obj = []
+    for l in lex(options['d']):
+        k, v = l
+        if k == 'identificador':
+
+            if len(obj) == 0: 
+                obj = []
+            else:
+                conjunto_obj.append(obj)
+                obj = []
+
+            if v == 'M':
+                obj.append(v)
+            if v == 'L':
+                obj.append(v)
+            if v == 'A':
+                obj.append(v)
+        if k == 'number':
+            obj.append(float(v))
+        if k == 'symbol':
+            conjunto_obj.append(obj)
+            
+    return conjunto_obj
+
 
 def addEllipseToCanvas(child_, objects):
     nodeName_ = child_.nodeName
@@ -179,17 +232,23 @@ def addEllipseToCanvas(child_, objects):
             options = {}
             for attr in list(attrs.keys()):
                 options[attr]=attrs[attr].value
-            print(options)
+            log.info(f"options = {options}")
             puntos = [ float(options['cx']), float(options['cy']),
                        float(options['rx']), float(options['ry'])]
+            ovalo = [ puntos[0]-puntos[2], puntos[1]-puntos[3],
+                      puntos[0]+puntos[2], puntos[1]+puntos[3]
+                      ]
+            log.info(f"Ovalo -- puntos: {ovalo}")
             opt = {}
             opt['outline'] = options['stroke']
-            # opt['fill'] = options['fill']
+            # opt['dash'] = (4, 1)
+            opt['fill'] = ''
             opt['width'] = float(options['stroke-width'])
             opt['tags'] = 'arc'
-            print(opt)
-            objects.create_oval(*puntos, opt)
-            print('create arco')
+            log.info(opt)
+            objects.create_oval(ovalo, opt)
+            log.info('create arco')
+
 
 def build(node_, objects):
     attrs = node_.attributes
@@ -226,7 +285,7 @@ def build(node_, objects):
             #if child_.nodeValue.startswith('\n'):
             #    print "TextNode starts with return:"+child_.nodeValue
             #else:
-#            print "TextNode is:"+child_.nodeValue
+            #    print "TextNode is:"+child_.nodeValue
             #object.setTextContent(child_.nodeValue)
             if child_.nodeValue != None and child_.nodeValue.strip() != '':
                 # print(len(child_.nodeValue))
@@ -238,6 +297,7 @@ def build(node_, objects):
         else:
             print("Some node:"+nodeName_+" value: "+child_.nodeValue)
     return objects
+
 
 def loadSvg(inFileName, canvas):
 
@@ -251,6 +311,7 @@ def loadSvg(inFileName, canvas):
     items = canvas.find_all()
     print(items)
     return canvas # rootObj
+
 
 if __name__ == '__main__':
     print('procesando ..')
